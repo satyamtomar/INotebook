f70dd209ca091444da16b7f37550dbc8ff314bda
satyamtomar <satyamtomar@users.noreply.github.com>
2021-09-27 22:42:32 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ connectToMongo();
 
 const app = express()
 const port = 3000
-
+app.use(express.json());
 //routes available
 
 app.use('/api/auth',require('./routes/auth'));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express=require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/',(req,res)=>{
-   obj={
-       a:'holy'
-       ,num:34
-   }
-    res.json(obj);
+//Creating a user using: POST "/api/auth/".Doesn't require auth
+router.post('/',(req,res)=>{
+console.log(req.body);
+const user=User(req.body);
+user.save();
+res.send(req.body);  
 })
 
 
