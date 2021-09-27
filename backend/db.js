c9011f09c24d354e838f95a940d47a07cc859587
satyamtomar <satyamtomar@users.noreply.github.com>
2021-09-27 13:41:31 +0530

    commit

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+const mongoURI="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("connection successful to mongoose");
+    })
+}
+
+module.exports=connectToMongo;
