b693cddcb6be56658328a972dea111af1b6b3a48
satyamtomar <satyamtomar@users.noreply.github.com>
2021-10-10 17:22:57 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import {useContext} from 'react';
-import NoteContext from '../context/notes/NoteContext';
-import { useEffect } from 'react';
+import React from "react";
+import { useContext } from "react";
+import NoteContext from "../context/notes/NoteContext";
+import { useEffect } from "react";
 const About = () => {
-    const a=useContext(NoteContext);
-    useEffect(()=>{
-      a.update()
-      // eslint-disable-next-line
-    },[])
-    return (
-        <div>
-            <h1>About{a.state.name} and class {a.state.class}</h1>
-        </div>
-    )
-}
+  const a = useContext(NoteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <div>
+      <h1>
+        About{a.state.name} and class {a.state.class}
+      </h1>
+    </div>
+  );
+};
 
-export default About
+export default About;
