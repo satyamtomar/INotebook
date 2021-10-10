20644f9a964b4728ae700a27296c832062b9c8ac
satyamtomar <satyamtomar@users.noreply.github.com>
2021-10-10 17:23:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import NoteState from './context/notes/NoteState';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 function App() {
   return (
     <>
-    <NoteState>
-  <Router>
-   <Navbar/>
-<Switch>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
         </Router>
-        </NoteState>
+      </NoteState>
     </>
   );
 }
