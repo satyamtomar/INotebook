b16bdb880d4845e5c1177ed63a3011cd47a5fa72
satyamtomar <satyamtomar@users.noreply.github.com>
2021-10-10 17:25:11 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
-const mongoose=require('mongoose');
-const mongoURI="mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
-const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connection successful to mongoose");
-    })
-}
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("connection successful to mongoose");
+  });
+};
 
-module.exports=connectToMongo;
+module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,20 @@
-const connectToMongo=require('./db');
-const express = require('express')
+const connectToMongo = require("./db");
+const express = require("express");
 
 connectToMongo();
 
-
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 app.use(express.json());
 //routes available
 
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/notes',require('./routes/notes'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
-app.get('/', (req, res) => {
-  res.send('Hello Satyam')
+app.get("/", (req, res) => {
+  res.send("Hello Satyam");
 });
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
