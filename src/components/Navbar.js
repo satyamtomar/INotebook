0ffe68de2d235e65b1dc014140e004080edb5db5
satyamtomar <satyamtomar@users.noreply.github.com>
2021-10-11 11:22:31 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,10 +10,10 @@ const Navbar = () => {
   
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            INotes
+            INotebook
           </Link>
           <button
             className="navbar-toggler"
