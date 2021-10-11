794e65f02a964146e216ad84adb842c920ee6898
satyamtomar <satyamtomar@users.noreply.github.com>
2021-10-11 16:38:29 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -3,47 +3,44 @@
 import React from "react";
 import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
-import YouTubeIcon from "@material-ui/icons/YouTube";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import LinkedinIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 const About = () => {
   const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
+    window.location = "https://www.linkedin.com/in/satyam-tomar-0873a91b0/";
   };
   return (
     <div className="aboutSection">
       <div></div>
       <div className="aboutSectionGradient"></div>
       <div className="aboutSectionContainer">
-        <Typography component="h1">About Us</Typography>
+        <Typography component="h1">About Me</Typography>
 
         <div>
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              src="satyam.jpg"
               alt="Founder"
             />
-            <Typography>Abhishek Singh</Typography>
+            <Typography>Satyam Tomar</Typography>
             <Button onClick={visitInstagram} color="primary">
-              Visit Instagram
+              Visit me
             </Button>
             <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
-            </span>
+your own cloud notebook...write your notes and save it on cloud server  </span>
           </div>
           <div className="aboutSectionContainer2">
-            <Typography component="h2">Our Brands</Typography>
+            <Typography component="h2">My Social Media</Typography>
             <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              href="https://github.com/satyamtomar"
               target="blank"
             >
-              <YouTubeIcon className="youtubeSvgIcon" />
+              <GitHubIcon className="youtubeSvgIcon" />
             </a>
 
-            <a href="https://instagram.com/meabhisingh" target="blank">
-              <LinkedinIcon className="instagramSvgIcon" />
+            <a href="https://www.linkedin.com/in/satyam-tomar-0873a91b0/" target="blank">
+              <LinkedInIcon className="instagramSvgIcon" />
             </a>
           </div>
         </div>
