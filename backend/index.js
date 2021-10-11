7e58a78e585adc133e88d4cc2ef8086356d8307b
satyamtomar <satyamtomar@users.noreply.github.com>
2021-10-11 12:01:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,5 +16,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`INotes listening at http://localhost:${port}`);
 });
