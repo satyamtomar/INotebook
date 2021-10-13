45b74f56c713c91e23368e2d0cda52076f0f175c
satyamtomar <satyamtomar@users.noreply.github.com>
2021-10-13 12:44:58 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
 function App() {
   return (
     <>
       <NoteState>
         <Router>
           <Navbar />
+          <Alert message="satyamtomar" />
           <div className="container">
           <Switch>
             <Route exact path="/about">
