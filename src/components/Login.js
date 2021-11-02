3f26ee8d1d6c77fbff2da752dca26e249eecd707
satyamtomar <satyamtomar@users.noreply.github.com>
2021-11-02 13:30:05 +0530

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Login = () => {
     return (
         <div>
-            I am Login
+            <form>
+  <div class="mb-3">
+    <label for="email" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label for="password" className="form-label">Password</label>
+    <input type="password" className="form-control"name="password" id="password"/>
+  </div>
+  <div className="mb-3 form-check">
+  </div>
+  <button type="submit" className="btn btn-dark">Submit</button>
+</form>
         </div>
     )
 }
