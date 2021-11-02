3ec1252465901e58cec12df314663f0ca7d42b9f
satyamtomar <satyamtomar@users.noreply.github.com>
2021-11-02 13:20:47 +0530

    update

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-const Alert = (props) => {
+function Alert(props) {
+    const capitalize =(word)=>{
+        const lower=word.toLowerCase();
+        return lower.charAt(0).toUpperCase()+lower.slice(1);
+    }
     return (
-        <div>
-            <div className="alert alert-primary my-3" role="alert">
- {props.message}
+        <div style={{height:'50px'}}>
+       { props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+  <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg} 
+ 
+</div>}
 </div>
-        </div>
     )
 }
 
